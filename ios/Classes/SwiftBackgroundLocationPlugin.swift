99c28a8b6462530d1505271e3cfe09e1a4363f10
Akiko Hiura <akiko.hiura@justincase-tech.com>
2023-04-10 11:34:36 +0900

    fix location settings for ios - add isRunning check

--- ios/Classes/SwiftBackgroundLocationPlugin.swift
+++ ios/Classes/SwiftBackgroundLocationPlugin.swift
@@ -2,16 +2,19 @@ import Flutter
 import UIKit
 import CoreLocation
 
-public class SwiftBackgroundLocationPlugin: NSObject, FlutterPlugin, CLLocationManagerDelegate {
+public class SwiftBackgroundLocationPlugin: NSObject, FlutterPlugin, CLLocationManagerDelegate, FlutterStreamHandler {
     static var locationManager: CLLocationManager?
     static var channel: FlutterMethodChannel?
-    
+    private var eventSink: FlutterEventSink?
+
     public static func register(with registrar: FlutterPluginRegistrar) {
         let instance = SwiftBackgroundLocationPlugin()
         
         SwiftBackgroundLocationPlugin.channel = FlutterMethodChannel(name: "com.almoullim.background_location/methods", binaryMessenger: registrar.messenger())
         registrar.addMethodCallDelegate(instance, channel: SwiftBackgroundLocationPlugin.channel!)
         SwiftBackgroundLocationPlugin.channel?.setMethodCallHandler(instance.handle)
+        let eventChannel = FlutterEventChannel(name: "com.almoullim.background_location/events", binaryMessenger: registrar.messenger())
+        eventChannel.setStreamHandler(instance)
     }
     
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
@@ -23,21 +26,23 @@ public class SwiftBackgroundLocationPlugin: NSObject, FlutterPlugin, CLLocationM
         if #available(iOS 11.0, *) {
             SwiftBackgroundLocationPlugin.locationManager?.showsBackgroundLocationIndicator = true;
         }
+        if #available(iOS 14.0, *) {
+            SwiftBackgroundLocationPlugin.locationManager?.desiredAccuracy = kCLLocationAccuracyReduced
+        } else {
+            SwiftBackgroundLocationPlugin.locationManager?.desiredAccuracy = kCLLocationAccuracyThreeKilometers
+        }
         SwiftBackgroundLocationPlugin.locationManager?.pausesLocationUpdatesAutomatically = false
-
-        SwiftBackgroundLocationPlugin.channel?.invokeMethod("location", arguments: "method")
+        SwiftBackgroundLocationPlugin.locationManager?.activityType = .other
 
         if (call.method == "start_location_service") {
-            SwiftBackgroundLocationPlugin.channel?.invokeMethod("location", arguments: "start_location_service")
-            
             let args = call.arguments as? Dictionary<String, Any>
             let distanceFilter = args?["distance_filter"] as? Double
             SwiftBackgroundLocationPlugin.locationManager?.distanceFilter = distanceFilter ?? 0
-            
             SwiftBackgroundLocationPlugin.locationManager?.startUpdatingLocation() 
         } else if (call.method == "stop_location_service") {
-            SwiftBackgroundLocationPlugin.channel?.invokeMethod("location", arguments: "stop_location_service")
-            SwiftBackgroundLocationPlugin.locationManager?.stopUpdatingLocation()
+           SwiftBackgroundLocationPlugin.locationManager?.stopUpdatingLocation()
+        } else if(call.method == "location_service_is_running") {
+            SwiftBackgroundLocationPlugin.locationManager?.startUpdatingLocation()
         }
         result(true)
     }
@@ -49,17 +54,33 @@ public class SwiftBackgroundLocationPlugin: NSObject, FlutterPlugin, CLLocationM
     }
     
     public func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let locationData = locations.last else {
+            return
+        }
+
         let location = [
-            "speed": locations.last!.speed,
-            "altitude": locations.last!.altitude,
-            "latitude": locations.last!.coordinate.latitude,
-            "longitude": locations.last!.coordinate.longitude,
-            "accuracy": locations.last!.horizontalAccuracy,
-            "bearing": locations.last!.course,
-            "time": locations.last!.timestamp.timeIntervalSince1970 * 1000,
+            "speed": locationData.speed,
+            "altitude": locationData.altitude,
+            "latitude": locationData.coordinate.latitude,
+            "longitude": locationData.coordinate.longitude,
+            "accuracy": locationData.horizontalAccuracy,
+            "bearing": locationData.course,
+            "time": locationData.timestamp.timeIntervalSince1970 * 1000,
             "is_mock": false
         ] as [String : Any]
 
         SwiftBackgroundLocationPlugin.channel?.invokeMethod("location", arguments: location)
+        eventSink?(location)
+    }
+
+    public func onListen(withArguments arguments: Any?,
+                                 eventSink: @escaping FlutterEventSink) -> FlutterError? {
+        self.eventSink = eventSink
+        return nil
+    }
+
+    public func onCancel(withArguments arguments: Any?) -> FlutterError? {
+        eventSink = nil
+        return nil
     }
 }
